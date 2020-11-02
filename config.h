c1c6eee676b0917f303740a6c340f266d3d3bd7e
Smoodle <migwwe@gmail.com>
2020-11-02 23:22:48 +0000

    Added new gitignore and changed border width

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 5;       	/* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
